5dbb8ff7cf931fbfd5a6bdc435ba4687974d09e0
Benjamin Alden <79180236+benjaminalden33@users.noreply.github.com>
2021-08-24 06:31:10 -0400

    Add files via upload

--- elevatorBot.py
+++ elevatorBot.py
@@ -1,1 +1,17 @@
-#hello
+import discord
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.auther == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('hello')
+
+client.run(NzA0ODU2MTg0NDczMTI0OTA0.XqjOeQ.-tTVsX90uAsbJJhE-p333asu2nM)
